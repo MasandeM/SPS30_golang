b60770f969e060d9c2965a72adcb04834229d833
MasandeM <masande.mti+github@gmail.com>
2024-06-28 21:02:42 +0100

    improve error handling and remove unused vars

--- sps30.go
+++ sps30.go
@@ -2,7 +2,8 @@ package sps30
 
 import (
 	"encoding/binary"
-	"log"
+	"errors"
+	"fmt"
 	"math"
 
 	"go.bug.st/serial"
@@ -13,14 +14,6 @@ const shdlcStop = 0x7e
 const shdlcFrameMaxTxFrameSize = 520 // start/stop + (4 header + 255 data) * 2 for byte stuffing
 const shdlcFrameMaxRxFrameSize = 522 // start/stop + (5 header + 255 data) * 2 because of byte stuffing
 
-const shdlcErrNoData = -1
-const shdlcErrMissingStart = -2
-const shdlcErrMissingStop = -3
-const shdlcErrCRCMismatch = -4
-const shdlcErrEncodingError = -5
-const shdlcErrTxIncomplete = -6
-const shdlcErrFrameTooLong = -7
-const shdlcRxHeaderSize = 4
 const peripheralAddr = 0
 const cmdReadVersion = 0xd1
 const cmdStartMeasurement = 0x00
@@ -28,7 +21,15 @@ const CmdReadMeasurement = 0x03
 const CmdWakeUp = 0x11
 const ErrNotEnoughData = -1
 
-var subcmdMeasurementStart = [2]byte{0x01, 0x03}
+// Define the error map
+var errorMap = map[int]string{
+	1:  "Wrong data length for this command (too much or little data)",
+	2:  "Unknown command",
+	3:  "No access right for command",
+	4:  "Illegal command parameter or parameter out of allowed range",
+	40: "Internal function argument out of range",
+	67: "Command not allowed in current state",
+}
 
 // header of a frame sent from the sps30 sensor
 type shdlcRxHeader struct {
@@ -77,37 +78,37 @@ func New(uart serial.Port) Device {
 }
 
 // Wakeup switches the device from sleep-mode to idle mode
-func (d *Device) Wakeup() {
+func (d *Device) Wakeup() error {
 	data := []byte{0xff}
 
 	_, err := d.uart.Write(data)
 	if err != nil {
-		log.Fatal(err)
+		return errors.New("unable to WakeUP SPS30 device")
 	}
 	rx_header := shdlcRxHeader{}
 	rdata := make([]byte, 0)
 
-	d.SHDLCTransmitReceive(peripheralAddr, CmdWakeUp, 0, nil, 0, &rx_header, &rdata)
+	return d.SHDLCTransmitReceive(peripheralAddr, CmdWakeUp, 0, nil, 0, &rx_header, &rdata)
 }
 
 // ReadVersion populates a VersionInfo struct with version information about the firmware, hardware, and SHDLC protocol
-func (d *Device) ReadVersion(version_info *VersionInfo) int {
+func (d *Device) ReadVersion(version_info *VersionInfo) error {
 
 	rx_header := shdlcRxHeader{}
 	data := make([]byte, 7)
 
-	ret := d.SHDLCTransmitReceive(peripheralAddr, cmdReadVersion, 0, nil, uint8(len(data)), &rx_header, &data)
+	err := d.SHDLCTransmitReceive(peripheralAddr, cmdReadVersion, 0, nil, uint8(len(data)), &rx_header, &data)
 
-	if ret != 0 {
-		return ret
+	if err != nil {
+		return fmt.Errorf("could not read version info from device: %v", err)
 	}
 
 	if int(rx_header.data_len) != len(data) {
-		return ErrNotEnoughData
+		return errors.New("did not receive enough data from device when reading version info")
 	}
 
 	if rx_header.state != 0 {
-		return int(rx_header.state) //original code uses masking. Is this needed?
+		return fmt.Errorf("invalid results received from device. Reason: %v", errorMap[int(rx_header.state)])
 	}
 
 	version_info.FirmwarMajor = data[0]
@@ -116,11 +117,11 @@ func (d *Device) ReadVersion(version_info *VersionInfo) int {
 	version_info.SHDLCMajor = data[5]
 	version_info.SHDLCMinor = data[6]
 
-	return 0
+	return nil
 }
 
 // StartMeasurement puts the SPS30 in Measure-mode.
-func (d *Device) StartMeasurement() int {
+func (d *Device) StartMeasurement() error {
 	rx_header := shdlcRxHeader{}
 	subcmd := []byte{0x01, 0x03}
 	data := make([]byte, 0)
@@ -128,18 +129,23 @@ func (d *Device) StartMeasurement() int {
 	return d.SHDLCTransmitReceive(peripheralAddr, cmdStartMeasurement, uint8(len(subcmd)), subcmd, 0, &rx_header, &data)
 }
 
-func (d *Device) ReadMeasurement(measurement *Measurement) int {
+// ReadMeasurement reads measurement values  from sps30 device
+func (d *Device) ReadMeasurement(measurement *Measurement) error {
 	rx_header := shdlcRxHeader{}
 	data := make([]byte, 40)
 
-	ret := d.SHDLCTransmitReceive(peripheralAddr, CmdReadMeasurement, 0, nil, uint8(len(data)), &rx_header, &data)
+	err := d.SHDLCTransmitReceive(peripheralAddr, CmdReadMeasurement, 0, nil, uint8(len(data)), &rx_header, &data)
 
-	if ret != 0 {
-		return ret
+	if err != nil {
+		return fmt.Errorf("could not read measurement from device: %v", err)
 	}
 
 	if int(rx_header.data_len) != len(data) {
-		return ErrNotEnoughData
+		return errors.New("did not receive enough data from device when reading measurements")
+	}
+
+	if rx_header.state != 0 {
+		return fmt.Errorf("invalid results received from device. Reason: %v", errorMap[int(rx_header.state)])
 	}
 
 	(*measurement).Mc1p0 = bytesFloat32(data[0:4])
@@ -153,11 +159,7 @@ func (d *Device) ReadMeasurement(measurement *Measurement) int {
 	(*measurement).Nc10p0 = bytesFloat32(data[32:36])
 	(*measurement).TypicalParticleSize = bytesFloat32(data[36:40])
 
-	if rx_header.state != 0 {
-		return int(rx_header.state)
-	}
-
-	return 0
+	return nil
 }
 
 func bytesFloat32(bytes []byte) float32 {
@@ -166,37 +168,37 @@ func bytesFloat32(bytes []byte) float32 {
 	return float
 }
 
+// SHDLCTransmitReceive transmits SHDLC Frame to the device, and populates rx_header and rx_data with the response.
 func (d *Device) SHDLCTransmitReceive(addr uint8,
 	cmd uint8, tx_data_len uint8,
 	tx_data []byte,
 	max_rx_data_len uint8,
 	rx_header *shdlcRxHeader,
-	rx_data *[]byte) int {
+	rx_data *[]byte) error {
 	// transcieve (transmit then receive) and SHDLC Frame
 
-	ret := d.shdlc_tx(addr, cmd, tx_data_len, tx_data)
-	if ret != 0 {
-		log.Fatal("Failed to send data to sensor")
-		return ret
+	err := d.shdlcTx(addr, cmd, tx_data_len, tx_data)
+	if err != nil {
+		return fmt.Errorf("shdlc XCV failed: %v", err)
 	}
 
-	return d.shdlc_rx(int(max_rx_data_len), rx_header, rx_data)
+	return d.shdlcRx(int(max_rx_data_len), rx_header, rx_data)
 }
 
-func (d *Device) shdlc_tx(addr uint8, cmd uint8, data_len uint8, data []byte) int {
+func (d *Device) shdlcTx(addr uint8, cmd uint8, data_len uint8, data []byte) error {
 
 	var tx_frame = [shdlcFrameMaxTxFrameSize]byte{}
 	len := 0
 
-	crc := shdlc_crc(addr+cmd, data_len, data)
+	crc := shdlcCRC(addr+cmd, data_len, data)
 
 	tx_frame[len] = shdlcStart
 	len += 1
 
-	len = shdlc_stuff_data(1, []byte{addr}, &tx_frame, len)
-	len = shdlc_stuff_data(1, []byte{cmd}, &tx_frame, len)
-	len = shdlc_stuff_data(1, []byte{data_len}, &tx_frame, len)
-	len = shdlc_stuff_data(int(data_len), data, &tx_frame, len)
+	len = stuffData(1, []byte{addr}, &tx_frame, len)
+	len = stuffData(1, []byte{cmd}, &tx_frame, len)
+	len = stuffData(1, []byte{data_len}, &tx_frame, len)
+	len = stuffData(int(data_len), data, &tx_frame, len)
 
 	tx_frame[len] = crc
 	len += 1
@@ -206,14 +208,13 @@ func (d *Device) shdlc_tx(addr uint8, cmd uint8, data_len uint8, data []byte) in
 
 	_, err := d.uart.Write(tx_frame[:len])
 	if err != nil {
-		log.Fatal(err)
-		return -1
+		return fmt.Errorf("unable send data to sensor: %v", err)
 	}
 
-	return 0
+	return nil
 }
 
-func (d *Device) shdlc_rx(max_data_len int, rx_header *shdlcRxHeader, data *[]byte) int {
+func (d *Device) shdlcRx(max_data_len int, rx_header *shdlcRxHeader, data *[]byte) error {
 
 	rx_frame := make([]byte, shdlcFrameMaxRxFrameSize)
 	header_index := 0
@@ -222,42 +223,41 @@ func (d *Device) shdlc_rx(max_data_len int, rx_header *shdlcRxHeader, data *[]by
 
 	frame_len, err := d.uart.Read(rx_frame)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("failed to read data from sensor: %v", err)
 	}
 
 	if frame_len < 1 || rx_frame[0] != shdlcStart {
-		return shdlcErrMissingStart
+		return errors.New("missing SHDLC Start byte in Rx frame")
 	}
 
 	// get Frame Header
 	header_index = 1
-	header_index = unstuff_byte(rx_frame, header_index, &(*rx_header).addr)
-	header_index = unstuff_byte(rx_frame, header_index, &(*rx_header).cmd)
-	header_index = unstuff_byte(rx_frame, header_index, &(*rx_header).state)
-	header_index = unstuff_byte(rx_frame, header_index, &(*rx_header).data_len)
+	header_index = unstuffByte(rx_frame, header_index, &(*rx_header).addr)
+	header_index = unstuffByte(rx_frame, header_index, &(*rx_header).cmd)
+	header_index = unstuffByte(rx_frame, header_index, &(*rx_header).state)
+	header_index = unstuffByte(rx_frame, header_index, &(*rx_header).data_len)
 
 	data_index = header_index
 	i := 0
 	for data_index < frame_len-2 && i < max_data_len {
-		data_index = unstuff_byte(rx_frame, data_index, &(*data)[i])
+		data_index = unstuffByte(rx_frame, data_index, &(*data)[i])
 		i += 1
 	}
 
-	crc = shdlc_crc(rx_header.addr+rx_header.cmd+rx_header.state, rx_header.data_len, *data)
+	crc = shdlcCRC(rx_header.addr+rx_header.cmd+rx_header.state, rx_header.data_len, *data)
 	if crc != rx_frame[data_index] {
-		return shdlcErrCRCMismatch
+		return errors.New("mismatch in CRC")
 	}
 	data_index += 1
 
 	if data_index >= frame_len || rx_frame[data_index] != shdlcStop {
-		log.Fatal("Missing SHDLC STOP byte")
-		return shdlcErrCRCMismatch
+		return errors.New("missing SHDLC STOP byte")
 	}
 
-	return 0
+	return nil
 }
 
-func unstuff_byte(data []byte, index int, value *uint8) int {
+func unstuffByte(data []byte, index int, value *uint8) int {
 
 	if data[index] == 0x7d {
 		switch data[index+1] {
@@ -282,7 +282,7 @@ func unstuff_byte(data []byte, index int, value *uint8) int {
 	}
 }
 
-func shdlc_stuff_data(data_len int, data []byte, stuffed_data *[shdlcFrameMaxTxFrameSize]byte, index int) int {
+func stuffData(data_len int, data []byte, stuffed_data *[shdlcFrameMaxTxFrameSize]byte, index int) int {
 
 	for i := 0; i < data_len; i++ {
 		switch data[i] {
@@ -302,7 +302,7 @@ func shdlc_stuff_data(data_len int, data []byte, stuffed_data *[shdlcFrameMaxTxF
 	return index
 }
 
-func shdlc_crc(header_sum uint8, data_len uint8, data []byte) uint8 {
+func shdlcCRC(header_sum uint8, data_len uint8, data []byte) uint8 {
 
 	sum := header_sum + data_len
 
